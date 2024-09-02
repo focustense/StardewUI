fc99873bc48567073d02d503681b63b4314b2961
focustense <focustense@protonmail.com>
2024-09-02 13:36:34 -0400

    Absorb `ScrollableFrameView` into UI library.
    This is used in a few projects now and the implementations are similar enough to be able to define a single class to handle them all. It's a common enough scenario that it justifies a shared implementation.
    
    Since the code already depends on some of SDV's types, the actual sprites have finally been added directly to the project. There's a risk of scope creep, but should be OK as long as it is constrained to "sprites necessary for basic UI". Externalizing every single sprite for the frame view is just too onerous on the caller and generally pointless right now.

---
+++ src/ScrollableFrameView.cs
@@ -0,0 +1,239 @@
+﻿namespace StardewUI;
+
+/// <summary>
+/// Layout widget for a sectioned menu including a scrollable content area.
+/// </summary>
+/// <remarks>
+/// The primary content is always kept centered, with optional title, footer and sidebar (e.g. for
+/// navigation) available to decorate the view. Many if not most menus can be fully represented with
+/// this layout, as long as they do not have built-in subnavigation such as top-level tabs.
+/// </remarks>
+public class ScrollableFrameView : WrapperView
+{
+    /// <summary>
+    /// The primary content, which displays inside the menu frame and is clipped/scrollable.
+    /// </summary>
+    public IView? Content
+    {
+        get => contentContainer?.Content;
+        set
+        {
+            var _ = Root; // Ensure view created
+            contentContainer.Content = value;
+        }
+    }
+
+    /// <summary>
+    /// Optional footer to display below the <see cref="Content"/>.
+    /// </summary>
+    /// <remarks>
+    /// Footer layout can be any arbitrary size and will not push up the <see cref="Content"/>.
+    /// However, footers wider than the <c>Content</c> may cause problems.
+    /// </remarks>
+    public IView? Footer
+    {
+        get => footerContainer?.Children.FirstOrDefault();
+        set
+        {
+            var _ = Root; // Ensure view created
+            footerContainer.Children = value is not null ? [value] : [];
+        }
+    }
+
+    /// <summary>
+    /// Layout parameters to apply to the frame surrounding the <see cref="Content"/>.
+    /// </summary>
+    /// <remarks>
+    /// Determines the size of the scrollable area and should generally be one of the
+    /// <see cref="LayoutParameters.FixedSize"/> overloads, or at least have a fixed
+    /// <see cref="LayoutParameters.Height"/>.
+    /// </remarks>
+    public LayoutParameters FrameLayout
+    {
+        get => contentFrame?.Layout ?? default;
+        set
+        {
+            var _ = Root; // Ensure view created
+            contentFrame.Layout = value;
+        }
+    }
+
+    /// <summary>
+    /// Optional content to display to the left of the <see cref="Content"/> frame.
+    /// </summary>
+    /// <remarks>
+    /// Typically used for navigation or other contextual info. Max width is constrained to
+    /// <see cref="SidebarWidth"/> regardless of layout.
+    /// </remarks>
+    public IView? Sidebar
+    {
+        get => sidebarContainer?.Children[0];
+        set
+        {
+            var _ = Root; // Ensure view created
+            sidebarContainer.Children = value is not null ? [value] : [];
+        }
+    }
+
+    /// <summary>
+    /// Maximum width of the sidebar area.
+    /// </summary>
+    /// <remarks>
+    /// To keep the primary content centered, the same dimension must be applied to the scrollbar's
+    /// container, so the width must be specified ahead of time. This acts as a maximum width; the
+    /// actual sidebar does not have to fill this space (it will be right-aligned in that case), but
+    /// larger views may clip or overflow.
+    /// </remarks>
+    public int SidebarWidth
+    {
+        get => sidebarWidth;
+        set
+        {
+            if (sidebarWidth == value)
+            {
+                return;
+            }
+            sidebarWidth = value;
+            if (IsViewCreated)
+            {
+                sidebarContainer.Layout = new()
+                {
+                    Width = Length.Px(sidebarWidth),
+                    Height = Length.Content(),
+                };
+                scrollbar.Layout = new()
+                {
+                    Width = Length.Px(sidebarWidth),
+                    Height = Length.Stretch(),
+                };
+            }
+        }
+    }
+
+    /// <summary>
+    /// Title to display above the <see cref="Content"/>.
+    /// </summary>
+    /// <remarks>
+    /// All titles are displayed as a <see cref="Banner"/>.
+    /// </remarks>
+    public string? Title
+    {
+        get => banner?.Text;
+        set
+        {
+            var _ = Root; // Ensure view created
+            banner.Text = value ?? "";
+            banner.Visibility = !string.IsNullOrEmpty(value) ? Visibility.Visible : Visibility.Hidden;
+        }
+    }
+
+    private int sidebarWidth;
+
+    // Initialized in CreateView
+    private Banner banner = null!;
+    private ScrollContainer contentContainer = null!;
+    private Frame contentFrame = null!;
+    private Panel footerContainer = null!;
+    private Scrollbar scrollbar = null!;
+    private Panel sidebarContainer = null!;
+    private Lane scrollingLayout = null!;
+
+    public override bool Measure(Vector2 availableSize)
+    {
+        var wasDirty = base.Measure(availableSize);
+        if (wasDirty)
+        {
+            footerContainer.Margin = new(Top: (int)MathF.Ceiling(scrollingLayout.OuterSize.Y));
+        }
+        return wasDirty;
+    }
+
+    public override void OnWheel(WheelEventArgs e)
+    {
+        if (e.Handled || scrollbar.Container is not ScrollContainer container)
+        {
+            return;
+        }
+        switch (e.Direction)
+        {
+            case Direction.North when container.Orientation == Orientation.Vertical:
+            case Direction.West when container.Orientation == Orientation.Horizontal:
+                e.Handled = container.ScrollBackward();
+                break;
+            case Direction.South when container.Orientation == Orientation.Vertical:
+            case Direction.East when container.Orientation == Orientation.Horizontal:
+                e.Handled = container.ScrollForward();
+                break;
+        }
+        if (e.Handled)
+        {
+            Game1.playSound("shwip");
+        }
+    }
+
+    protected override IView CreateView()
+    {
+        banner = new Banner()
+        {
+            Layout = LayoutParameters.FitContent(),
+            Margin = new(Top: -85),
+            Padding = new(12),
+            Background = UiSprites.BannerBackground,
+            BackgroundBorderThickness =
+                (UiSprites.BannerBackground.FixedEdges ?? Edges.NONE)
+                * (UiSprites.BannerBackground.SliceSettings?.Scale ?? 1),
+        };
+        contentContainer = new ScrollContainer()
+        {
+            Name = "ContentScrollContainer",
+            Peeking = 16,
+            ScrollStep = 64,
+            Layout = LayoutParameters.Fill(),
+        };
+        contentFrame = new Frame()
+        {
+            Name = "ContentFrame",
+            Background = UiSprites.MenuBackground,
+            Border = UiSprites.MenuBorder,
+            BorderThickness = UiSprites.MenuBorderThickness,
+            Margin = new(Top: -20),
+            Content = contentContainer,
+        };
+        sidebarContainer = new Panel()
+        {
+            Layout = new() { Width = Length.Px(sidebarWidth), Height = Length.Content() },
+            HorizontalContentAlignment = Alignment.End,
+        };
+        scrollbar = new Scrollbar(
+            UiSprites.SmallUpArrow,
+            UiSprites.SmallDownArrow,
+            UiSprites.ScrollBarTrack,
+            UiSprites.VerticalScrollThumb
+        )
+        {
+            Name = "ContentPageScroll",
+            Layout = new() { Width = Length.Px(sidebarWidth), Height = Length.Stretch() },
+            Margin = new(Top: 10, Bottom: 20),
+        };
+        scrollbar.Container = contentContainer;
+        scrollingLayout = new Lane()
+        {
+            Name = "ScrollableFrameScrollingLayout",
+            Layout = LayoutParameters.FitContent(),
+            Children = [sidebarContainer, contentFrame, scrollbar],
+            ZIndex = 1,
+        };
+        footerContainer = new Panel()
+        {
+            Name = "ScrollableFrameFooter",
+            Layout = LayoutParameters.FitContent(),
+        };
+        return new Panel()
+        {
+            Name = "ScrollableFrameContentLayout",
+            Layout = LayoutParameters.FitContent(),
+            HorizontalContentAlignment = Alignment.Middle,
+            Children = [banner, scrollingLayout, footerContainer],
+        };
+    }
+}
---
+++ src/UiSprites.cs
@@ -0,0 +1,78 @@
+﻿namespace StardewUI;
+
+/// <summary>
+/// Included game sprites that are required for many UI/menu widgets.
+/// </summary>
+public static class UiSprites
+{
+    /// <summary>
+    /// Background for the a banner or "scroll" style text, often used for menu/dialogue titles.
+    /// </summary>
+    public static Sprite BannerBackground =>
+        new(
+            Game1.mouseCursors,
+            SourceRect: new(325, 318, 25, 18),
+            FixedEdges: new(12, 0),
+            SliceSettings: new(Scale: 4)
+        );
+
+    /// <summary>
+    /// Border/background sprite for an individual control, such as a button. Less prominent than
+    /// <see cref="MenuBorder"/>.
+    /// </summary>
+    public static Sprite ControlBorder =>
+        new(Game1.menuTexture, SourceRect: new(0, 256, 60, 60), FixedEdges: new(16));
+
+    /// <summary>
+    /// Background used for the in-game menu, not including borders.
+    /// </summary>
+    public static Sprite MenuBackground => new(Game1.menuTexture, SourceRect: new(64, 128, 64, 64));
+
+    /// <summary>
+    /// Modified 9-slice sprite used for the menu border, based on menu "tiles". Used for drawing the outer border of an
+    /// entire menu UI.
+    /// </summary>
+    public static Sprite MenuBorder =>
+        new(
+            Game1.menuTexture,
+            SourceRect: new(0, 0, 256, 256),
+            FixedEdges: new(64),
+            SliceSettings: new(CenterX: 128, CenterY: 128, EdgesOnly: true)
+        );
+
+    /// <summary>
+    /// The actual distance from the outer edges of the <see cref="MenuBorder"/> sprite to where the actual "border"
+    /// really ends, in terms of pixels. The border tiles are quite large, so this tends to be needed in order to
+    /// determine where the content should go without adding a ton of extra padding.
+    /// </summary>
+    public static Edges MenuBorderThickness => new(36, 36, 40, 36);
+
+    /// <summary>
+    /// Background for the scroll bar track (which the thumb is inside).
+    /// </summary>
+    public static Sprite ScrollBarTrack =>
+        new(
+            Game1.mouseCursors,
+            SourceRect: new(403, 383, 6, 6),
+            FixedEdges: new(2),
+            SliceSettings: new(Scale: 4)
+        );
+
+    /// <summary>
+    /// Small up arrow, typically used for scroll bars.
+    /// </summary>
+    public static Sprite SmallUpArrow => new(Game1.mouseCursors, SourceRect: new(421, 459, 11, 12));
+
+    /// <summary>
+    /// Small down arrow, typically used for scroll bars.
+    /// </summary>
+    public static Sprite SmallDownArrow =>
+        new(Game1.mouseCursors, SourceRect: new(421, 472, 11, 12));
+
+    /// <summary>
+    /// Thumb sprite used for vertical scroll bars.
+    /// </summary>
+    public static Sprite VerticalScrollThumb =>
+        new(Game1.mouseCursors, SourceRect: new(435, 463, 6, 10));
+}
+
