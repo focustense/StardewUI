150ebb35166ebb910d514b7c893ba79b5263544b
focustense <focustense@protonmail.com>
2024-09-09 22:22:22 -0400

    Add support for Sprite animations (cycling an `Image` through many sprites).
    Works similar to property-based animations, and includes some support for a start delay, since many game animations should "rest" for a period.
    
    Makes `AnimationRunner` public as it cannot run itself, it needs to have its `Tick` method run from the game's update loop (SMAPI `UpdateTicked`).

--- src/Animation.cs
+++ src/Animation.cs
@@ -263,9 +263,86 @@ public class Animator<T, V> : IAnimator where T : class
 public record Animation<T>(T StartValue, T EndValue, TimeSpan Duration);
 
 /// <summary>
+/// Animates the sprite of an <see cref="Image"/>, using equal duration for all frames in a list.
+/// </summary>
+public class SpriteAnimator : IAnimator
+{
+    /// <summary>
+    /// Duration of each frame.
+    /// </summary>
+    public TimeSpan FrameDuration { get; set; }
+
+    /// <summary>
+    /// Frames to animate through.
+    /// </summary>
+    public IReadOnlyList<Sprite> Frames { get; set; } = [];
+
+    /// <summary>
+    /// Whether or not to pause animation. If <c>true</c>, the animator will hold at the current position and not
+    /// progress until set to <c>false</c> again. Does not affect the <see cref="CurrentAnimation"/>.
+    /// </summary>
+    public bool Paused { get; set; }
+
+    /// <summary>
+    /// Delay before advancing from the first frame to the next frames.
+    /// </summary>
+    /// <remarks>
+    /// Repeats on every loop, but only applies to the first frame of each loop.
+    /// </remarks>
+    public TimeSpan StartDelay { get; set; }
+
+    private readonly WeakReference<Image> imageRef;
+
+    private TimeSpan delayElapsed;
+    private TimeSpan elapsed;
+
+    public SpriteAnimator(Image image)
+    {
+        imageRef = new(image);
+        AnimationRunner.Register(this);
+    }
+
+    public void Tick(TimeSpan elapsed)
+    {
+        if (Paused || FrameDuration == TimeSpan.Zero || Frames.Count == 0 || !imageRef.TryGetTarget(out var image))
+        {
+            return;
+        }
+        if (this.elapsed == TimeSpan.Zero && delayElapsed < StartDelay)
+        {
+            delayElapsed += elapsed;
+            if (Frames.Count > 0)
+            {
+                image.Sprite = Frames[0];
+            }
+            if (delayElapsed < StartDelay)
+            {
+                return;
+            }
+        }
+        delayElapsed = TimeSpan.Zero;
+        var totalDuration = FrameDuration * Frames.Count;
+        this.elapsed += elapsed;
+        if (this.elapsed > totalDuration)
+        {
+            this.elapsed = StartDelay > TimeSpan.Zero
+                ? TimeSpan.Zero
+                : TimeSpan.FromTicks(this.elapsed.Ticks % totalDuration.Ticks);
+        }
+        var frameIndex = (int)(this.elapsed.TotalMilliseconds / FrameDuration.TotalMilliseconds);
+        image.Sprite = Frames[frameIndex];
+    }
+
+    bool IAnimator.IsValid()
+    {
+        return imageRef.TryGetTarget(out _);
+    }
+}
+
+/// <summary>
 /// Central registry for view animations, meant to be driven from the game loop.
 /// </summary>
-internal static class AnimationRunner
+public static class AnimationRunner
 {
     private static readonly LinkedList<IAnimator> animators = [];
 
@@ -274,7 +351,11 @@ internal static class AnimationRunner
         animators.AddLast(animator);
     }
 
-    internal static void Tick(TimeSpan elapsed)
+    /// <summary>
+    /// Handles a game tick.
+    /// </summary>
+    /// <param name="elapsed">Time elapsed since last tick.</param>
+    public static void Tick(TimeSpan elapsed)
     {
         var node = animators.First;
         while (node is not null)
