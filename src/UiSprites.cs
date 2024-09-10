53b5fba1f77fdd95eadcbfa43c31598562585eaf
focustense <focustense@protonmail.com>
2024-09-09 23:34:21 -0400

    Add horizontal divider sprites to UI sprites.
    These are commonly used in "main" menus.

--- src/UiSprites.cs
+++ src/UiSprites.cs
@@ -66,6 +66,23 @@ public static class UiSprites
     public static Edges MenuBorderThickness => new(36, 36, 40, 36);
 
     /// <summary>
+    /// Modified 9-slice sprite used for the menu's horizontal divider, meant to be drawn over top of the
+    /// <see cref="MenuBorder"/> to denote separate "sub-panels" or "sections" of the menu to group logically very
+    /// different menu functions (as opposed to lines on a grid).
+    /// </summary>
+    public static Sprite MenuHorizontalDivider => new(
+        Game1.menuTexture,
+        SourceRect: new(0, 64, 256, 64),
+        FixedEdges: new(64, 0),
+        SliceSettings: new(CenterX: 128));
+
+    /// <summary>
+    /// Margin adjustment to apply to content adjacent to a <see cref="MenuHorizontalDivider"/> to make content flush
+    /// with the border; adjusts for internal sprite padding.
+    /// </summary>
+    public static Edges MenuHorizontalDividerMargin => new(-36, -20, -40, -20);
+
+    /// <summary>
     /// Background for the scroll bar track (which the thumb is inside).
     /// </summary>
     public static Sprite ScrollBarTrack =>
