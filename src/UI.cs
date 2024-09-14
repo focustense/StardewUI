309a967d960ced404cf5f42907898ac4691356be
focustense <focustense@protonmail.com>
2024-09-13 23:54:31 -0400

    Add entry point for SMAPI/logging init.
    Previously mods would set `Logger.Monitor` directly for just logging. They can still technically do that, but from now on they are going to be required to call `UI.Initialize` which also sets up logging (so the former call can be removed).
    
    With the ongoing keybinding work, having access to the SMAPI input helper (for input suppression) is too important to let go of; the vanilla APIs are just way too clunky.

---
+++ src/UI.cs
@@ -0,0 +1,39 @@
+﻿using StardewModdingAPI;
+
+namespace StardewUI;
+
+/// <summary>
+/// Entry point for Stardew UI. Must be called from <see cref="Mod.Entry(IModHelper)"/>.
+/// </summary>
+public static class UI
+{
+    /// <summary>
+    /// Helper for game input.
+    /// </summary>
+    internal static IInputHelper InputHelper => EnsureInitialized(() => modHelper.Input);
+
+    private static IModHelper modHelper = null!;
+
+    /// <summary>
+    /// Initialize the framework.
+    /// </summary>
+    /// <param name="helper">Helper for the calling mod.</param>
+    /// <param name="monitor">SMAPI logging helper.</param>
+    public static void Initialize(IModHelper helper, IMonitor monitor)
+    {
+        modHelper = helper;
+        Logger.Monitor = monitor;
+    }
+
+    private static T EnsureInitialized<T>(Func<T> selector)
+    {
+        if (modHelper is null)
+        {
+            throw new InvalidOperationException(
+                "StardewUI has not been initialized. Ensure you've called UI.Initialize(helper) from your mod's "
+                    + "Entry method."
+            );
+        }
+        return selector();
+    }
+}
