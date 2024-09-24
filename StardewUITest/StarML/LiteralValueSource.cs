47e22772406b612a56d260eb76bb1b75abd9a4a1
focustense <focustense@protonmail.com>
2024-09-24 18:22:08 -0400

    Refactor: Split up all the "source" and asset related types.

---
+++ StardewUITest/StarML/LiteralValueSource.cs
@@ -0,0 +1,20 @@
+﻿namespace StardewUITest.StarML;
+
+/// <summary>
+/// Value source that uses the literal (text) value of an attribute.
+/// </summary>
+/// <param name="value">The attribute value.</param>
+public class LiteralValueSource(string value) : IValueSource<string>
+{
+    public string Value => value;
+
+    /// <inheritdoc />
+    /// <remarks>
+    /// As implemented on <see cref="LiteralValueSource"/>, always returns <c>false</c> as there can never be any change
+    /// that requires an update.
+    /// </remarks>
+    public bool Update()
+    {
+        return false;
+    }
+}
