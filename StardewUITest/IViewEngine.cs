1a8bfcccc8c5354fc3e0f4f69eef81de267c763b
focustense <focustense@protonmail.com>
2024-09-26 22:35:44 -0400

    Add integration/test for framework in test mod.

---
+++ StardewUITest/IViewEngine.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using StardewValley.Menus;
+
+namespace StardewUI.Framework;
+
+/// <summary>
+/// Public API for StardewUI, abstracting away all implementation details of views and trees.
+/// </summary>
+public interface IViewEngine
+{
+    /// <summary>
+    /// Creates a menu from the StarML stored in a game asset, as provided by a mod via SMAPI or Content Patcher.
+    /// </summary>
+    /// <remarks>
+    /// Does not make the menu active. To show it, use <see cref="Game1.activeClickableMenu"/> or equivalent.
+    /// </remarks>
+    /// <param name="assetName">The name of the asset in the content pipeline, e.g. <c>Mods/MyMod/UI/MyMenu</c>.</param>
+    /// <param name="context">The context, or "model", for the menu's view, which holds any data-dependent values.
+    /// <b>Note:</b> The type must implement <see cref="INotifyPropertyChanged"/> in order for any changes to this data
+    /// to be automatically reflected in the UI.</param>
+    /// <returns>A menu object which can be shown using the game's standard menu APIs such as
+    /// <see cref="Game1.activeClickableMenu"/>.</returns>
+    IClickableMenu CreateMenuFromAsset(string assetName, object? context = null);
+
+    /// <summary>
+    /// Creates a menu from arbitrary markup.
+    /// </summary>
+    /// <remarks>
+    /// <b>Warning:</b> Ad-hoc menus created this way cannot be cached, nor patched by other mods. Most mods should not
+    /// use this API except for testing/experimentation.
+    /// </remarks>
+    /// <param name="markup">The markup in StarML format.</param>
+    /// <param name="context">The context, or "model", for the menu's view, which holds any data-dependent values.
+    /// <b>Note:</b> The type must implement <see cref="INotifyPropertyChanged"/> in order for any changes to this data
+    /// to be automatically reflected in the UI.</param>
+    /// <returns>A menu object which can be shown using the game's standard menu APIs such as
+    /// <see cref="Game1.activeClickableMenu"/>.</returns>
+    IClickableMenu CreateMenuFromMarkup(string markup, object? context = null);
+}
--- StardewUITest/ModEntry.cs
+++ StardewUITest/ModEntry.cs
@@ -1,6 +1,7 @@
 ﻿using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewUI;
+using StardewUI.Framework;
 using StardewValley;
 
 namespace StardewUITest;
@@ -10,20 +11,51 @@ internal sealed class ModEntry : Mod
     // Initialized in Entry
     private ModConfig config = null!;
 
+    // Initialized in GameLaunched
+    private IViewEngine viewEngine = null!;
+
+    private string ViewMarkup =>
+        @"<lane orientation=""vertical"" horizontal-content-alignment=""middle"">
+            <banner background={{@Mods/StardewUI/Sprites/BannerBackground}} background-border-thickness=""48,0"" padding=""12"" text={{HeaderText}} />
+            <frame background={{@Mods/StardewUI/Sprites/ControlBorder}} margin=""0,16,0,0"" padding=""32,24"">
+                <lane orientation=""vertical"" horizontal-content-alignment=""middle"">
+                    <label font=""dialogue"" text=""Hello, world!"" margin=""0,0,0,8"" />
+                    <label text=""This is an example paragraph."" />
+                    <panel margin=""0,8,0,0"" horizontal-content-alignment=""middle"" vertical-content-alignment=""middle"">
+                        <image layout=""stretch"" fit=""stretch"" sprite={{@Mods/StardewUI/Sprites/MenuSlotInset}} />
+                        <image layout=""64px 64px"" margin=""8"" sprite={{ItemData}} />
+                    </panel>
+                </lane>
+            </frame>
+        </lane>";
+
     public override void Entry(IModHelper helper)
     {
         I18n.Init(helper.Translation);
         UI.Initialize(helper, Monitor);
         config = helper.ReadConfig<ModConfig>();
 
+        helper.Events.GameLoop.GameLaunched += GameLoop_GameLaunched;
         helper.Events.Input.ButtonPressed += Input_ButtonPressed;
+    }
+
+    private void GameLoop_GameLaunched(object? sender, GameLaunchedEventArgs e)
+    {
+        viewEngine = Helper.ModRegistry.GetApi<IViewEngine>("focustense.StardewUI")!;
     }
 
     private void Input_ButtonPressed(object? sender, ButtonPressedEventArgs e)
     {
         if (Context.IsPlayerFree && e.Button == SButton.F8)
         {
-            Game1.activeClickableMenu = new TestMenu();
+            // Game1.activeClickableMenu = new TestMenu();
+
+            var context = new
+            {
+                HeaderText = "Example Menu Title",
+                ItemData = ItemRegistry.GetData("(O)117"),
+            };
+            Game1.activeClickableMenu = viewEngine.CreateMenuFromMarkup(ViewMarkup, context);
         }
     }
 }
