e5a13667d0888e5ec460b86eeeecb1f7177fb9ac
focustense <focustense@protonmail.com>
2024-09-26 22:33:18 -0400

    Add public API for external instantiation of views/menus.

--- StardewUI.Framework/ModEntry.cs
+++ StardewUI.Framework/ModEntry.cs
@@ -1,13 +1,51 @@
-﻿namespace StardewUI.Framework;
+﻿using StardewModdingAPI.Events;
+using StardewUI.Framework.Api;
+using StardewUI.Framework.Binding;
+using StardewUI.Framework.Content;
+using StardewUI.Framework.Converters;
+using StardewUI.Framework.Sources;
+
+namespace StardewUI.Framework;
 
 internal sealed class ModEntry : Mod
 {
     // Initialized in Entry
     private ModConfig config = null!;
+    private ViewEngine viewEngine = null!;
 
     public override void Entry(IModHelper helper)
     {
         I18n.Init(helper.Translation);
+        UI.Initialize(helper, Monitor);
         config = helper.ReadConfig<ModConfig>();
+
+        viewEngine = CreateViewEngine();
+
+        helper.Events.Content.AssetRequested += Content_AssetRequested;
+    }
+
+    public override object? GetApi()
+    {
+        return viewEngine;
+    }
+
+    private void Content_AssetRequested(object? sender, AssetRequestedEventArgs e)
+    {
+        if (e.NameWithoutLocale.StartsWith(UiSpriteProvider.AssetNamePrefix))
+        {
+            e.LoadFrom(() => UiSpriteProvider.GetSprite(e.Name.Name), AssetLoadPriority.Exclusive);
+        }
+    }
+
+    private ViewEngine CreateViewEngine()
+    {
+        var viewFactory = new ViewFactory();
+        var assetCache = new AssetCache(Helper.GameContent, Helper.Events.Content);
+        var valueSourceFactory = new ValueSourceFactory(assetCache);
+        var valueConverterFactory = new ValueConverterFactory();
+        var attributeBindingFactory = new AttributeBindingFactory(valueSourceFactory, valueConverterFactory);
+        var viewBinder = new ReflectionViewBinder(attributeBindingFactory);
+        var viewNodeFactory = new ViewNodeFactory(viewFactory, viewBinder);
+        return new ViewEngine(Helper.GameContent, viewNodeFactory);
     }
 }
