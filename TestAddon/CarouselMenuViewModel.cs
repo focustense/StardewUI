feaffb9457ea0ad637e4dafd43bb9d7c019f4b97
focustense <focustense@protonmail.com>
2024-11-05 14:48:44 -0500

    Add carousel example as demo of custom views.
    Uses the add-on system to register a `Carousel` view type which slides a selected child into/out of view (always centered).
    
    Also adds another custom converter to handle the easings, and provide an alternate example of why/when to use that feature and how it interacts with custom views.

---
+++ TestAddon/CarouselMenuViewModel.cs
@@ -0,0 +1,70 @@
+﻿using PropertyChanged.SourceGenerator;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.ItemTypeDefinitions;
+
+namespace StardewUITestAddon;
+
+internal partial class CarouselMenuViewModel
+{
+    public int HeaderIndex => SelectedPageIndex + 1;
+    public List<CarouselMenuPage> Pages { get; set; } = [];
+
+    [Notify] private int selectedPageIndex;
+
+    public bool HandleButtonPress(SButton button)
+    {
+        switch (button)
+        {
+            case SButton.RightShoulder:
+                if (SelectedPageIndex < (Pages.Count - 1))
+                {
+                    SelectedPageIndex++;
+                    return true;
+                }
+                break;
+            case SButton.LeftShoulder:
+                if (SelectedPageIndex > 0)
+                {
+                    SelectedPageIndex--;
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+}
+
+internal class CarouselMenuPage(string title = "", TrainCarType carType = default, TrainCovering covering = default, IReadOnlyList<PurchasableItem>? items = null)
+{
+    public string Title { get; } = title;
+
+    public TrainCarType CarType { get; } = carType;
+
+    public TrainCovering Covering { get; } = covering;
+
+    public IReadOnlyList<PurchasableItem> Items { get; set; } = items ?? [];
+}
+
+internal enum TrainCarType { Passenger, Freight }
+
+internal enum TrainCovering { None, Minerals, Boxes }
+
+internal class PurchasableItem(ParsedItemData data, int price)
+{
+    public static PurchasableItem Load(string id, float discount)
+    {
+        var itemData = ItemRegistry.GetDataOrErrorItem(id);
+        var item = ItemRegistry.Create(id);
+        return new(itemData, (int)MathF.Round(item.salePrice() * (1 - discount)));
+    }
+
+    public static IReadOnlyList<PurchasableItem> LoadAll(IEnumerable<string> ids, float discount)
+    {
+        return ids.Select(id => Load(id, discount)).ToList();
+    }
+
+    public ParsedItemData Data { get; } = data;
+    public string DisplayName => Data.DisplayName;
+    public int Price { get; } = price;
+}
