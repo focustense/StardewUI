62e26135f26ef85942507474f10754ca91506f45
focustense <focustense@protonmail.com>
2024-11-13 14:21:40 -0500

    Reduce temporal coupling in drop-down lists.
    Fixes some inconsistent property-change notifications, and adds logic to try to hold onto the `SelectedValue` until it becomes available in the list or until it is completely overridden by something else, like setting SelectedIndex or SelectedValue to a different value.
    
    #44

--- Framework/Views/DynamicDropDownList.cs
+++ Framework/Views/DynamicDropDownList.cs
@@ -56,6 +56,10 @@ internal class DynamicDropDownList : DecoratorView
             {
                 MaybeRecreateAdapter();
                 adapter.Options = value;
+                if (selectedOption.IsDirty)
+                {
+                    TrySetSelectedOption(selectedOption.Value?.Value);
+                }
             }
         }
     }
@@ -82,15 +86,21 @@ internal class DynamicDropDownList : DecoratorView
         {
             if (selectedOption.SetIfChanged(value))
             {
-                if (adapter.IsValidOption(value?.Value))
-                {
-                    adapter.SelectedOption = value?.Value;
-                    if (adapter.SelectedIndex >= 0 || !selectedIndex.IsDirty)
-                    {
-                        selectedIndex.Value = adapter.SelectedIndex;
-                    }
-                }
+                TrySetSelectedOption(value?.Value);
             }
+        }
+    }
+
+    private void TrySetSelectedOption(object? value)
+    {
+        if (!adapter.IsValidOption(value))
+        {
+            return;
+        }
+        adapter.SelectedOption = value;
+        if (adapter.SelectedIndex >= 0 || !selectedIndex.IsDirty)
+        {
+            selectedIndex.Value = adapter.SelectedIndex;
         }
     }
 
