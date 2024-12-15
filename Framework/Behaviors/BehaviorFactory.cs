8a68113cc7b1ac90959990551cc9200503b51c57
focustense <focustense@protonmail.com>
2024-12-15 16:06:14 -0500

    Behaviors: Add interfaces/base classes and factories.
    #68

---
+++ Framework/Behaviors/BehaviorFactory.cs
@@ -0,0 +1,50 @@
+﻿namespace StardewUI.Framework.Behaviors;
+
+/// <summary>
+/// A behavior factory based on per-name delegates. Can be used as a base class for other factories.
+/// </summary>
+public class BehaviorFactory : IBehaviorFactory
+{
+    private readonly Dictionary<string, Func<string, IViewBehavior>> nameFactories = new(
+        StringComparer.OrdinalIgnoreCase
+    );
+
+    /// <inheritdoc />
+    public IViewBehavior CreateBehavior(string name, string argument)
+    {
+        return nameFactories.TryGetValue(name, out var factory)
+            ? factory(argument)
+            : throw new ArgumentException($"Unsupported behavior type: {name}", nameof(name));
+    }
+
+    /// <summary>
+    /// Registers a behavior for a given name using the behavior's default parameterless constructor.
+    /// </summary>
+    /// <remarks>
+    /// Used for behaviors that do not take arguments, only data.
+    /// </remarks>
+    /// <typeparam name="TBehavior">The behavior type.</typeparam>
+    /// <param name="name">The markup name used to create to the <typeparamref name="TBehavior"/> type.</param>
+    public void Register<TBehavior>(string name)
+        where TBehavior : IViewBehavior, new()
+    {
+        nameFactories.Add(name, _ => new TBehavior());
+    }
+
+    /// <summary>
+    /// Registers a behavior for a given name using a delegate function.
+    /// </summary>
+    /// <param name="name">The markup name used to create this type of behavior.</param>
+    /// <param name="factory">Delegate function that accepts the construction argument (if any) and creates the
+    /// corresponding behavior.</param>
+    public void Register(string name, Func<string, IViewBehavior> factory)
+    {
+        nameFactories.Add(name, factory);
+    }
+
+    /// <inheritdoc />
+    public bool SupportsName(string name)
+    {
+        return nameFactories.ContainsKey(name);
+    }
+}
---
+++ Framework/Behaviors/IViewBehavior.cs
@@ -0,0 +1,39 @@
+﻿namespace StardewUI.Framework.Behaviors;
+
+/// <summary>
+/// Provides methods for attaching arbitrary data-dependent behavior to a view.
+/// </summary>
+/// <remarks>
+/// Add-ons should normally not use this interface directly, and instead derive from
+/// <see cref="ViewBehavior{TView, TData}"/> for type safety.
+/// </remarks>
+public interface IViewBehavior : IDisposable
+{
+    /// <summary>
+    /// The type of data that the behavior accepts in <see cref="SetData(object)"/>.
+    /// </summary>
+    Type DataType { get; }
+
+    /// <summary>
+    /// The type of view that the behavior accepts in <see cref="SetView(IView)"/>.
+    /// </summary>
+    Type ViewType { get; }
+
+    /// <summary>
+    /// Updates the behavior's current data.
+    /// </summary>
+    /// <param name="data">The new data.</param>
+    void SetData(object data);
+
+    /// <summary>
+    /// Updates the behavior's attached view.
+    /// </summary>
+    /// <param name="view">The new view.</param>
+    void SetView(IView view);
+
+    /// <summary>
+    /// Runs on every update tick.
+    /// </summary>
+    /// <param name="elapsed">Time elapsed since the last update.</param>
+    void Update(TimeSpan elapsed);
+}
