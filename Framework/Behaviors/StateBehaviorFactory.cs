0c9255da5edbb6237d6d0be98d85df237042a1b3
focustense <focustense@protonmail.com>
2024-12-18 18:51:03 -0500

    Adds factory and behavior for hover states.
    This has now been tested to work with a simple use case, `+hover:transform`. Transitions are still not implemented but we now have the two major components that need to be connected _with_ the transition - the transform itself and the behavior to trigger it on/off.
    
    #59

---
+++ Framework/Behaviors/StateBehaviorFactory.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Concurrent;
+using System.Reflection;
+using StardewUI.Framework.Binding;
+using StardewUI.Framework.Descriptors;
+
+namespace StardewUI.Framework.Behaviors;
+
+/// <summary>
+/// Factory for creating behaviors that apply single-property overrides on state transitions, such as
+/// <c>hover:transform</c>.
+/// </summary>
+public class StateBehaviorFactory : IBehaviorFactory
+{
+    private delegate IViewBehavior BehaviorCreator(string propertyName);
+
+    private record BehaviorPropertyKey(string Name, Type PropertyType);
+
+    private record BehaviorViewKey(Type ViewType, string ArgumentName);
+
+    private static readonly MethodInfo CreateHoverBehaviorMethod = typeof(StateBehaviorFactory).GetMethod(
+        nameof(CreateHoverBehavior),
+        BindingFlags.Static | BindingFlags.NonPublic
+    )!;
+    private static readonly ConcurrentDictionary<BehaviorPropertyKey, BehaviorCreator> creatorsByProperty = [];
+    private static readonly ConcurrentDictionary<BehaviorViewKey, BehaviorCreator> creatorsByViewArg = [];
+
+    /// <inheritdoc />
+    public IViewBehavior CreateBehavior(Type viewType, string name, string argument)
+    {
+        var propertyName = argument.AsSpan().ToUpperCamelCase();
+        var viewKey = new BehaviorViewKey(viewType, argument);
+        var creator = creatorsByViewArg.GetOrAdd(
+            viewKey,
+            _ =>
+            {
+                var viewDescriptor = DescriptorFactory.GetViewDescriptor(viewType);
+                var propertyDescriptor = viewDescriptor.GetProperty(propertyName);
+                var propertyKey = new BehaviorPropertyKey(name, propertyDescriptor.ValueType);
+                return creatorsByProperty.GetOrAdd(
+                    propertyKey,
+                    _ => GetCreatorDelegate(propertyKey.Name, propertyKey.PropertyType)
+                );
+            }
+        );
+        return creator(propertyName);
+    }
+
+    /// <inheritdoc />
+    public bool SupportsName(string name)
+    {
+        // Keep in sync with GetCreatorDelegate.
+        return name.Equals("hover", StringComparison.OrdinalIgnoreCase);
+    }
+
+    private static BehaviorCreator GetCreatorDelegate(string stateName, Type valueType)
+    {
+        // Keep in sync with SupportsName.
+        return stateName.ToLowerInvariant() switch
+        {
+            "hover" => CreateHoverBehaviorMethod.MakeGenericMethod(valueType).CreateDelegate<BehaviorCreator>(),
+            _ => throw new ArgumentException($"Unrecognized state name '{stateName}'.", nameof(stateName)),
+        };
+    }
+
+    private static IViewBehavior CreateHoverBehavior<TValue>(string propertyName)
+    {
+        return new HoverStateBehavior<TValue>(propertyName);
+    }
+}
