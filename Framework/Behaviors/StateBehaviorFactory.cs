7c815872bae7882e0005513e9dad13e90fdd2fee
focustense <focustense@protonmail.com>
2024-12-19 17:38:23 -0500

    Add missing `StateBehaviorFactory` registration.
    This was originally in, then removed to keep the project buildable in a partial commit, then should have been added back but wasn't.
    
    #59

--- Framework/Behaviors/StateBehaviorFactory.cs
+++ Framework/Behaviors/StateBehaviorFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Concurrent;
 using System.Reflection;
+using StardewUI.Animation;
 using StardewUI.Framework.Binding;
 using StardewUI.Framework.Descriptors;
 
@@ -9,6 +10,9 @@ namespace StardewUI.Framework.Behaviors;
 /// Factory for creating behaviors that apply single-property overrides on state transitions, such as
 /// <c>hover:transform</c>.
 /// </summary>
+/// <remarks>
+/// Also handles transitions, which follow a similar creation mechanism.
+/// </remarks>
 public class StateBehaviorFactory : IBehaviorFactory
 {
     private delegate IViewBehavior BehaviorCreator(string propertyName);
@@ -17,18 +21,24 @@ public class StateBehaviorFactory : IBehaviorFactory
 
     private record BehaviorViewKey(Type ViewType, string ArgumentName);
 
-    private static readonly MethodInfo CreateHoverBehaviorMethod = typeof(StateBehaviorFactory).GetMethod(
-        nameof(CreateHoverBehavior),
-        BindingFlags.Static | BindingFlags.NonPublic
+    private static readonly MethodInfo CreateHoverBehaviorMethod = GetFactoryMethod(nameof(CreateHoverBehavior))!;
+    private static readonly MethodInfo CreateTransitionBehaviorMethod = GetFactoryMethod(
+        nameof(CreateTransitionBehavior)
     )!;
+
     private static readonly ConcurrentDictionary<BehaviorPropertyKey, BehaviorCreator> creatorsByProperty = [];
     private static readonly ConcurrentDictionary<BehaviorViewKey, BehaviorCreator> creatorsByViewArg = [];
+
+    private static MethodInfo? GetFactoryMethod(string name)
+    {
+        return typeof(StateBehaviorFactory).GetMethod(name, BindingFlags.Static | BindingFlags.NonPublic);
+    }
 
     /// <inheritdoc />
     public IViewBehavior CreateBehavior(Type viewType, string name, string argument)
     {
         var propertyName = argument.AsSpan().ToUpperCamelCase();
-        var viewKey = new BehaviorViewKey(viewType, argument);
+        var viewKey = new BehaviorViewKey(viewType, name + ':' + argument);
         var creator = creatorsByViewArg.GetOrAdd(
             viewKey,
             _ =>
@@ -49,21 +59,35 @@ public class StateBehaviorFactory : IBehaviorFactory
     public bool SupportsName(string name)
     {
         // Keep in sync with GetCreatorDelegate.
-        return name.Equals("hover", StringComparison.OrdinalIgnoreCase);
+        return name.Equals("hover", StringComparison.OrdinalIgnoreCase)
+            || name.Equals("transition", StringComparison.OrdinalIgnoreCase);
     }
 
     private static BehaviorCreator GetCreatorDelegate(string stateName, Type valueType)
     {
         // Keep in sync with SupportsName.
-        return stateName.ToLowerInvariant() switch
+        var method = stateName.ToLowerInvariant() switch
         {
-            "hover" => CreateHoverBehaviorMethod.MakeGenericMethod(valueType).CreateDelegate<BehaviorCreator>(),
+            "hover" => CreateHoverBehaviorMethod.MakeGenericMethod(valueType),
+            "transition" => CreateTransitionBehaviorMethod.MakeGenericMethod(valueType),
             _ => throw new ArgumentException($"Unrecognized state name '{stateName}'.", nameof(stateName)),
         };
+        return method.CreateDelegate<BehaviorCreator>();
     }
 
     private static IViewBehavior CreateHoverBehavior<TValue>(string propertyName)
     {
         return new HoverStateBehavior<TValue>(propertyName);
+    }
+
+    private static IViewBehavior CreateTransitionBehavior<TValue>(string propertyName)
+    {
+        var lerp =
+            Lerps.Get<TValue>()
+            ?? throw new BindingException(
+                $"Cannot transition property '{propertyName}' with type {typeof(TValue).FullName} because the value type "
+                    + "does not have a defined interpolation (Lerp) function."
+            );
+        return new TransitionBehavior<TValue>(propertyName, lerp);
     }
 }
