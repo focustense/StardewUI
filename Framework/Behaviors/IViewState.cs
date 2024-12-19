8e306b42f1713d2889ee6c5c06a6a038901a68da
focustense <focustense@protonmail.com>
2024-12-19 17:44:05 -0500

    Add transition behavior and supporting factory and conversion methods.
    Includes a somewhat unrelated change to provide read-only access to the entire state list (per property). This was originally part of the `TransitionBehavior` implementation, then removed in cleanup, but since the read-only access may be useful for future behaviors and carries no risk on its own, it's kept here anyway.
    
    Updates the grid example with a hover transition as a demo.
    
    #59

--- Framework/Behaviors/IViewState.cs
+++ Framework/Behaviors/IViewState.cs
@@ -8,6 +8,25 @@ namespace StardewUI.Framework.Behaviors;
 public interface IViewState
 {
     /// <summary>
+    /// Retrieves the default value for a given property.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The default value is the value that will be used when there are no states for that property, i.e. when
+    /// <see cref="GetProperty"/> returns <c>null</c> for the specified <paramref name="propertyName"/> or when the
+    /// property's states are empty.
+    /// </para>
+    /// <para>
+    /// Defaults are real-time; if the property is linked via data binding, then the default value is the value that is
+    /// currently bound.
+    /// </para>
+    /// </remarks>
+    /// <typeparam name="T">The property value type.</typeparam>
+    /// <param name="propertyName">The property name.</param>
+    /// <returns>The default value for the specified <paramref name="propertyName"/>.</returns>
+    public T GetDefaultValue<T>(string propertyName);
+
+    /// <summary>
     /// Gets the override states for the specified property, creating a new one if it does not already exist.
     /// </summary>
     /// <typeparam name="T">The property value type.</typeparam>
